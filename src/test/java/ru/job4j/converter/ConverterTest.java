146517cdeb82b3b77bb9a8f7ddb4b069af84c095
mvg79 <mvg79@mail.ru>
2022-03-28 13:16:18 +0400

    Модульные тесты. [#159435]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -3,7 +3,6 @@ package ru.job4j.converter;
 
 import org.junit.Test;
 import org.junit.Assert;
-import static org.junit.Assert.*;
 
 public class ConverterTest {
 
@@ -18,8 +17,8 @@ public class ConverterTest {
 
     @Test
     public void whenConvert200RblThen3dot3333Dlr() {
-        float in = 140;
-        float expected = 2.3333f;
+        float in = 200;
+        float expected = 3.3333f;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
